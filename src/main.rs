82aae3040fd47d6678855df7a33a4eba45ac9130
George Marcus <george.marcus922@gmail.com>
2024-09-29 23:14:17 +0300

    Simplify json conversion code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use std::process;
 use lisudoku_solver::types::SudokuConstraints;
-use lisudoku_ocr::{parse_image_at_url, CellCandidates};
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-struct OcrResultJson {
-  grid: String,
-  candidates: Vec<CellCandidates>,
-}
+use lisudoku_ocr::parse_image_at_url;
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
@@ -17,16 +11,16 @@ async fn main() {
       eprintln!("Usage: lisudoku_ocr path");
       process::exit(1);
     }
-};
+  };
 
   eprintln!("Parsing image at {}", image_url);
 
   let ocr_result = parse_image_at_url(&image_url).await.expect("Unable to parse sudoku grid from image");
-  
-  let ocr_result_json = OcrResultJson {
-    grid: SudokuConstraints::new(9, ocr_result.given_digits).to_import_string(),
-    candidates: ocr_result.candidates,
-  };
 
-  println!("{}", serde_json::to_string(&ocr_result_json).unwrap());
+  let ocr_result_json = json!({
+    "grid": SudokuConstraints::new(9, ocr_result.given_digits).to_import_string(),
+    "candidates": ocr_result.candidates,
+  });
+
+  println!("{}", &ocr_result_json);
 }
